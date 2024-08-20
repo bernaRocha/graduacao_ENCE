0ab71b84ee7bbdac1042e709b888f0ca1daf22d6
Bernardo Monteiro Rocha <mrochabernardo@gmail.com>
2024-08-20 20:01:28 -0300

    Create aula04_operadores_relacionais.py

---
+++ 02_introducao_programacao/aula04_operadores_relacionais.py
@@ -0,0 +1,23 @@
+'''
+Aula do dia 20-08-24
+
+- Conceito de função
+- Módulo math
+- Funções built-in
+- Operadores relacionais
+'''
+
+x = 0; y = 1
+v1 = (x < y)
+print(f'V1 = {v1}')
+v2 = (y % 2 == x) # resto da divisão
+print(f'V2 = {v2}')
+v3 = (y // 2 == x) # quociente da divisão
+print(f'V3 = {v3}')
+
+'''
+>>> True and False
+False
+>>> True or False
+True
+'''
