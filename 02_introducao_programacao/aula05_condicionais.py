0cedf975a1beb74bfee89dc2ed64420b2599ced4
Bernardo Monteiro Rocha <mrochabernardo@gmail.com>
2024-08-22 21:43:54 -0300

    Create aula05_condicionais.py

---
+++ 02_introducao_programacao/aula05_condicionais.py
@@ -0,0 +1,21 @@
+'''
+Aula do dia 22-08-24
+'''
+
+i = int(input('Digite sua idade: '))
+
+if i >= 18:
+    print('Você não é menor de idade')
+    
+print('FIM')
+
+#######
+
+i = int(input('Digite sua idade: '))
+
+if i >= 18:
+    print('Você não é menor de idade')
+else:
+    print('Você é menor')
+    
+print('FIM')
