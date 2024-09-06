121516ff65c6e5711a3f15c1f05bbc8bc4399e72
Bernardo Monteiro Rocha <mrochabernardo@gmail.com>
2024-09-05 21:51:19 -0300

    Aula de looping com for

---
+++ 02_introducao_programacao/aula08_for.py
@@ -0,0 +1,15 @@
+'''
+Aula do dia 05/0924
+Usando for faça um programa que peça o vlor de n
+e calcule h = 1/1 + 1/2 + ... + 1/n
+
+'''
+numerador = 1
+denominador = 1
+h = 0
+
+n = int(input('Digite um valor para n para calcular 1/1 + 1/2 + ... + 1/n: '))
+for denominador in range(1, n+1):
+    h += 1/denominador
+
+print(f'O valor de h é: {h}')
