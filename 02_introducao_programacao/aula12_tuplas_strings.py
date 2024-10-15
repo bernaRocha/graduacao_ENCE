83ceb50a2a2f3d91e2c67a366d08723c4821d03b
Bernardo Monteiro Rocha <mrochabernardo@gmail.com>
2024-10-15 19:44:27 -0300

    Create aula12_tuplas_strings.py

---
+++ 02_introducao_programacao/aula12_tuplas_strings.py
@@ -0,0 +1,14 @@
+'''
+Aula do dia 15-10-24
+Aula 12 do repositório: 
+
+'''
+
+t = (5, [1, 2], 15)
+t[1].append(666)
+print(t)
+
+
+for x, y in [(1,4), (2,5), (3,6)]:
+  print(f'x = {x}, y = {y}, x + y = {x + y}')
+# atribuição simultânea
