5d25a6e83619b8890980951df88246822bd593bd
bernardo monteiro rocha <mrochabernardo@gmail.com>
2024-10-27 11:14:11 -0300

    Adição da função de raíz cúbica

--- 00_funcoes.py
+++ 00_funcoes.py
@@ -2,7 +2,18 @@ def imprimir_matriz_bonita(matriz):
   '''
   Função para imprimir uma matriz de forma que fique melhor a compreensão dos números dentro da matriz
   '''
-    for i in range(len(matriz)):
-        for j in range(len(matriz[0])):
-            print(f"{matriz[i][j]:>6}", end="")
-        print()
+  for i in range(len(matriz)):
+    for j in range(len(matriz[0])):
+          print(f"{matriz[i][j]:>6}", end="")
+    print()
+
+
+def raiz_cubica(numero):
+    '''
+    Função para extrair a raíz cúbica de um número
+    '''
+    import numpy as np
+    a = np.cbrt(numero)
+    return f"A raiz cúbica de {numero} é: {round(a, 6)}"
+
+print(raiz_cubica(-27))  # A raiz cúbica de -27 é: -3.0
