5ec934ba199eb9c96dbba1cd93f0c607ae5c97e5
Bernardo Monteiro Rocha <mrochabernardo@gmail.com>
2024-10-31 21:47:53 -0300

    Update 00_funcoes.py

--- 00_funcoes.py
+++ 00_funcoes.py
@@ -37,3 +37,15 @@ valores = [1, 1.6, 1.75]
 for valor in valores:
     print(f'Polegadas: {converte_para_polegadas_pes(valor, 1)}')
     print(f'Pés: {converte_para_polegadas_pes(valor, 2)}')
+
+def f_prod(n):
+    '''
+    Recebe um inteiro e retorna o produto dos primeiros múltiplos de 5
+    exemplo: f_prod(3) -> retorna 5 * 10 * 15 = 750
+    '''
+    resp = 1
+    for i in range(1, n + 1):
+        resp *= 5 * i
+    return resp
+
+print(f_prod(3)) # 750
