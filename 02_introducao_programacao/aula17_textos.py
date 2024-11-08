93f250b3d18f5c57172f9e29045e3f7f467381c5
Bernardo Monteiro Rocha <mrochabernardo@gmail.com>
2024-11-07 21:22:33 -0300

    Update aula17_textos.py

--- 02_introducao_programacao/aula17_textos.py
+++ 02_introducao_programacao/aula17_textos.py
@@ -18,14 +18,16 @@ Vidur,39
 f.close()
 
 ######################################33
-
+# Exemplo 1: quantos países são da Europa e quantos são da América
+# Exmplo 2: quantas linhas tem o arquivo
 f = open("C:\\Program Files\\Python311\\Scripts\\paises.csv", encoding="utf-8")
+f.readline() # pula a linha do cabeçalho
 
-
-tot_europa = tot_america = 0
+tot_europa = tot_america = n = 0 # n é o número de linhas
 for linha in f:
     linha = linha.rstrip() # retira o \n do final de cada linha
-    colunas = linha.split(",")
+    colunas = linha.split(",") # transforma uma string em uma lista, separando os dados pela , - vírgula
+    n += 1
     print(colunas)
 
 
@@ -33,7 +35,7 @@ for linha in f:
     elif colunas[2] == "Europa": tot_europa += 1
 
 
-print(f"\nTotal América = {tot_america} e total Europa = {tot_europa}")
+print(f"\nTotal América = {tot_america} e total Europa = {tot_europa} e o total de países/ linhas = {n}")
 '''
 ['sigla', 'nome', 'continente', 'extensao']
 ['AR', 'Argentina', 'América', '2780']
@@ -44,4 +46,4 @@ print(f"\nTotal América = {tot_america} e total Europa = {tot_europa}")
 
 Total América = 2 e total Europa = 3
 '''
-f.close()
+f.close() # fecha o arquivo, libera a memória
