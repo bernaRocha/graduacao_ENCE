d92f7c9fea00705e6fa534b472ed791a7a9e5862
Bernardo <mrochabernardo@gmail.com>
2025-03-24 19:57:07 -0300

    Update aula_04_24_03.r

--- 03_estatistica_computacional/aula_04_24_03.r
+++ 03_estatistica_computacional/aula_04_24_03.r
@@ -9,6 +9,7 @@
 
 # Aula 04 > matrizes e arrays
   ### o índice comea no 1 e não no 0 como no python
+  ## Array - Pode ter 2 ou mais dimensões, consegue guardar mais informações
   # Para acessar A = [linha, coluna]
 
 A <- matrix(c(3,5,8, 10), nrow = 2, ncol=2)
@@ -25,3 +26,19 @@ B
 #     [,1] [,2] [,3]
 #[1,]    1    6   11
 #[2,]   16   21   26
+
+b <- matrix(x, nrow = 2)
+b
+#     [,1] [,2] [,3]
+#[1,]    1   11   21
+#[2,]    6   16   26
+
+dim(b) # [1] 2 3
+ncol(b) # 3
+nrow(b) # 2
+
+inflacao <- matrix(c(1.1, 0.95, 0.78, 1.25, 1.79, 1.89), ncol = 3, dimnames = list(c("ano_1", "ano_2"), c('Trim1','Trim2', 'Trim3')))
+inflacao
+#      Trim1 Trim2 Trim3
+#ano_1  1.10  0.78  1.79
+#ano_2  0.95  1.25  1.89
