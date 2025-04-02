14195f2da346592e27b57a1e02e628994ca28c74
Bernardo <mrochabernardo@gmail.com>
2025-04-02 20:38:03 -0300

    Update aula_06_02_04.r

---
+++ 03_estatistica_computacional/aula_06_02_04.r
@@ -0,0 +1,20 @@
+######## w3schools
+
+nome <- "Bernardo"
+idade <- 37
+curso_faculdade <- "estatística"
+paste("Meu nome é", nome, "e eu tenho", idade, "anos", ", eu estudo", curso_faculdade)
+
+ceiling(1.4) # arredonda para cima
+floor(1.4) # arredonda para baixo
+
+qwe <- "mais de uma
+linha, assim vem com o \n na hora de imprimir"
+qwe
+
+outra_string <- "para não ter o \n 
+tem que imprimir com a função cat()"
+cat(outra_string)
+
+################
+
