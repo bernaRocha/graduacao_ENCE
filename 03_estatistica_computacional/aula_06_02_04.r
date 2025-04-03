7da3637ca53954c589a75779a329a6967330a25f
Bernardo <mrochabernardo@gmail.com>
2025-04-02 21:05:37 -0300

    Update aula_06_02_04.r

--- 03_estatistica_computacional/aula_06_02_04.r
+++ 03_estatistica_computacional/aula_06_02_04.r
@@ -16,5 +16,24 @@ outra_string <- "para não ter o \n
 tem que imprimir com a função cat()"
 cat(outra_string)
 
+cat("qualquer coisa") # qualquer coisa
+
 ################
 
+combn(4,2) # de 1 até 4 e de dois em dois
+#     [,1] [,2] [,3] [,4] [,5] [,6]
+#[1,]    1    1    1    2    2    3
+#[2,]    2    3    4    3    4    4
+
+combn(c(1,5,6,8),2) # usando os números dentro de c, de dois em dois
+#     [,1] [,2] [,3] [,4] [,5] [,6]
+#[1,]    1    1    1    5    5    6
+#[2,]    5    6    8    6    8    8
+
+install.packages("partitions")
+library(partitions)
+
+perms(3)
+#[1,] 1 1 2 2 3 3
+#[2,] 2 3 1 3 1 2
+#[3,] 3 2 3 1 2 1
