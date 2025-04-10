18d3bf9cb7a32402b1425d03a9d79cd3450d07cb
Bernardo <mrochabernardo@gmail.com>
2025-04-09 21:38:18 -0300

    Update aula_08_09_04_cont_listas_dataframes.r

--- 03_estatistica_computacional/aula_08_09_04_cont_listas_dataframes.r
+++ 03_estatistica_computacional/aula_08_09_04_cont_listas_dataframes.r
@@ -50,3 +50,30 @@ lapply(lista, max)
 medias <- sapply(nota, mean)
 medias
 #[1] 6.4 6.2
+
+################ Pulei para a página 24
+
+idade <- sample(18:30, 10, replace = TRUE)
+idade
+# [1] 23 23 22 26 22 30 30 22 26 21
+
+periodo <- sample(1:8, 10, replace = TRUE)
+periodo
+# [1] 6 3 5 3 5 6 5 5 2 8
+
+raca <- sample(c("branca", "negra", "indigena", "parda", "amarela"), 10, replace = TRUE)
+raca
+# [1] "negra"    "branca"   "amarela"  "amarela"  "branca"   "parda"    "indigena" "indigena"
+# [9] "negra"    "amarela"
+
+cracumulado <- sample(50:100, 10, replace = TRUE) / 10
+cracumulado
+# [1] 7.1 6.8 6.4 5.2 6.7 9.9 9.1 8.7 6.7 8.2
+
+dados <- data.frame(idade, periodo, raca, cracumulado) # Cria o dataframe
+
+names(dados)
+#[1] "idade"       "periodo"     "raca"        "cracumulado"
+
+is.data.frame(dados) # Verifica se a variável é um dataframe
+# [1] TRUE
