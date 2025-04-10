318337aee55e319bffa5add4332dc97a16589d47
Bernardo <mrochabernardo@gmail.com>
2025-04-09 21:05:13 -0300

    Update and rename aula_07_09_04.r to aula_07_09_04_cont_listas_dataframes.r

--- 03_estatistica_computacional/aula_07_09_04_cont_listas_dataframes.r
+++ 03_estatistica_computacional/aula_07_09_04_cont_listas_dataframes.r
@@ -28,3 +28,25 @@ maior_nota
 
 #[[2]]
 #[1] 9
+
+lista <- list(matrix(sample(10, 4), 2), 1:10)
+lista
+#[[1]]
+#     [,1] [,2]
+#[1,]    8    5
+#[2,]    2    4
+
+#[[2]]
+# [1]  1  2  3  4  5  6  7  8  9 10
+
+lapply(lista, max)
+#[[1]]
+#[1] 8
+
+#[[2]]
+#[1] 10
+
+####### Em algumas situações a lista retornada por lapply pode ser transformada em um vetor ou em uma matriz mediante a utilização da função sapply
+medias <- sapply(nota, mean)
+medias
+#[1] 6.4 6.2
