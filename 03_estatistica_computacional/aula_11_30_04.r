f21dbb3a12f4e7b3939f2c2beb1088ebf9711df6
Bernardo <mrochabernardo@gmail.com>
2025-04-30 21:16:02 -0300

    Create aula_11_30_04.r

---
+++ 03_estatistica_computacional/aula_11_30_04.r
@@ -0,0 +1,19 @@
+###### Estruturas de controle e repetição e funcoes
+### https://aulasence.ibge.gov.br/Bacharelado/Daniela%20Polessa%20Paula/Estat%C3%ADstica%20Computacional%20I/Computacional_2022/Aula%207R.pdf
+
+x <- scan() # digitei 9 no console
+
+if(x >= 0)
+  print(sqrt(x + 10)) # [1] 4.358899
+else
+  print('raiz complexa')
+
+ifelse(x + 10 > 0, sqrt(x + 10), 'raiz complexa') # [1] 4.358899
+
+############ Ex 2
+
+## sqrt(x2 + 2) / sqrt(x1 ^ 2 - 4)
+
+if (x[2] >= -2 & (x[1] < -2)) #### O resto tá no aula7 notebook do repositório
+
+  
