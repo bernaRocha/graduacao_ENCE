b68c1c684167ccd7c56b78686a733282e04e3a52
Bernardo <mrochabernardo@gmail.com>
2025-05-05 15:07:46 -0300

    questão 2

---
+++ 03_estatistica_computacional/lista03.R
@@ -0,0 +1,10 @@
+#### 1
+
+#### 2 Construa uma lista com o seu nome, a sua idade, o nome do seu time e o seu número de irmãos. Em seguida, adicione nesta lista os nomes das disciplinas que você está cursando.
+
+dados <- list(nome <- "Bernardo ", idade <- 37, time <- "Botafogo", irmaos <- 0)
+materias <-c('Estatísticals públicas', 'Estatística computacional')
+perfil <- c(dados, materias)
+perfil
+
+##### 3 
