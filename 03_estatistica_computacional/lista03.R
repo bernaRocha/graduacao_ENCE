87950c397e28de3272e38945a864e2515a3faa3e
Bernardo <mrochabernardo@gmail.com>
2025-05-05 15:36:53 -0300

    Q 4 a) e b)

--- 03_estatistica_computacional/lista03.R
+++ 03_estatistica_computacional/lista03.R
@@ -17,7 +17,15 @@ M <- matrix(ncol = length(L), data = unlist(L), nrow = length(L[[1]]))
 d<-data.frame(candidatos=1:1000,n1=sample(0:10,1000,replace=TRUE), n2= sample(0:10,1000,replace=TRUE))
 
 # (a) Criar um novo campo correspondente à média de n1 e n2.
+library(dplyr)
+
+d <- d %>%
+  mutate(media = (d$n1 + d$n2) / 2)
+
 # (b) Determinar a nota média dos candidatos em relação à nota n1 e à nota n2.
+
+medias <- c(mean(d$n1), mean(d$n2)) # [1] 4.932 4.951
+
 # (c) Listar os números dos candidatos que tiveram a média menor do que cinco.
 # (d) Imprimir o total de candidatos que tiveram a média maior ou igual a sete.
 # (e) Excluir do data frame os candidatos que tiraram nota zero em pelo menos uma das provas.
