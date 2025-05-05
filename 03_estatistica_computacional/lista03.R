72ce8a54e26d1bda3cadd190387a8b50ccd5d845
Bernardo <mrochabernardo@gmail.com>
2025-05-05 16:19:46 -0300

    Questão 5

--- 03_estatistica_computacional/lista03.R
+++ 03_estatistica_computacional/lista03.R
@@ -27,5 +27,48 @@ d <- d %>%
 medias <- c(mean(d$n1), mean(d$n2)) # [1] 4.932 4.951
 
 # (c) Listar os números dos candidatos que tiveram a média menor do que cinco.
+
+menor_5 <- list(df_filtrado$candidatos)
+
 # (d) Imprimir o total de candidatos que tiveram a média maior ou igual a sete.
+
+media_igual_maior_7 <- subset(d, media >= 5)
+length(media_igual_maior_7$candidatos ) # [1] 572
+
 # (e) Excluir do data frame os candidatos que tiraram nota zero em pelo menos uma das provas.
+
+aprovados <- d %>%
+  filter(
+    !n1 == 0,
+    !n2 == 0
+  )
+
+aprovados
+length(aprovados$candidatos) # [1] 817
+
+#### 5) Construa um data frame que tenha os seguintes campos: sexo, grau de instrução, 
+# estado civil e renda. O data frame deve ter 40 registros. Considere as seguintes atribuições 
+# para a construção do data frame: para sexo (sample(c(“F”,M”),40,T)) para grau de instrução 
+# (sample(c(1,2,3),40,T), para o estado civil (sample(c(“S”,”C”,”D”,”V”),40,T) e renda 
+# (200*runif(40,0,1)). Uma vez construído o data frame: 
+
+#• Faça a tabulação para sexo e grau de instrução (freq. absoluta) 
+
+frequencia_absoluta <- table(dados$sexo, dados$grau_de_instrucao)
+frequencia_absoluta
+#     1  2  3
+#  F  6 10  3
+#  M  6  5 10
+
+#• Calcule a renda média 
+media_renda <- mean(dados$renda) # [1] 99.89947
+
+#• Calcule a moda do grau de instrução
+
+calcular_moda <- function(a) {
+  moda_a <- unique(a)
+  moda_a[which.max(tabulate(match(a, moda_a)))]
+}
+
+moda_df <- calcular_moda(dados$grau_de_instrucao)
+moda_df # [1] 2
